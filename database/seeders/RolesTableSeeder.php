48081a0b61317f906e3c9b07fced2bdaee17a3f2
httien-1001 <60769807+httien-1001@users.noreply.github.com>
2021-07-12 10:49:47 +0700

    add seed

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Route;
 
 class RolesTableSeeder extends Seeder
 {
@@ -13,6 +14,33 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $routes_admin = [];
+        foreach (Route::getRoutes() as $route){
+            $name = $route ->getName();
+            $check_include_admin = strpos($name,'admin');
+            $check_include_customer = strpos($name,'customer');
+            if($check_include_admin !== false || $check_include_customer !== false){
+                array_push($routes_admin, $route->getName());
+            }
+        }
+        $routes=json_encode($routes_admin);
+        $routes_reader = [];
+        foreach (Route::getRoutes() as $route){
+            $name = $route ->getName();
+            $check_include_admin = strpos($name,'admin');
+            $check_include_customer = strpos($name,'customer');
+            if($check_include_admin !== false || $check_include_customer !== false){
+                array_push($routes_reader, $route->getName());
+            }
+        }
+        $routes_reader=json_encode($routes_reader);
+        \DB::table('roles')->insert([
+            'name' => 'Admin',
+            'permissions' => $routes,
+        ]);
+        \DB::table('roles')->insert([
+            'name' => 'Member',
+            'permissions' => $routes_reader,
+        ]);
     }
 }
--- database/seeders/UsersRolesTableSeeder.php
+++ database/seeders/UsersRolesTableSeeder.php
@@ -13,6 +13,9 @@ class UsersRolesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \DB::table('users_roles')->insert([
+            'user_id' => 1,
+            'role_id' =>'1',
+        ]);
     }
 }
